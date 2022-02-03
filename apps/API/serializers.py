c4cef8273a15967b2a6aba1387d0a1ea2220d5c2
nicolasfrenay <*****>
2022-02-03 18:42:51 +0100

    list and detail views are working for customers

--- apps/API/serializers.py
+++ apps/API/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
-from apps.API.models import Customer
 from rest_framework.validators import UniqueTogetherValidator
+from apps.API.models import Customer
+from apps.authenticate.serializers import ListCustomUserSerializer, \
+    DetailCustomUserSerializer
 
 
-class CustomerSerializer(ModelSerializer):
-
+class CreateCustomerSerializer(ModelSerializer):
     class Meta:
         model = Customer
         fields = (
@@ -24,7 +25,33 @@ class CustomerSerializer(ModelSerializer):
         validators = [
             UniqueTogetherValidator(queryset=Customer.objects.all(),
                                     fields=['email'],
-                                    message= 'email already associated with '
-                                             'an existing customer'
-                            )
+                                    message='email already associated with '
+                                            'an existing customer'
+                                    )
         ]
+
+
+class ListCustomersSerializer(ModelSerializer):
+    sale_contact = SerializerMethodField()
+
+    class Meta:
+        model = Customer
+        fields = ('id', 'company', 'first_name', 'last_name', 'sale_contact')
+
+    def get_sale_contact(self, instance):
+        sale = instance.sale_contact
+        serializer = ListCustomUserSerializer(sale)
+        return serializer.data
+
+
+class DetailCustomersSerializer(ModelSerializer):
+    sale_contact = SerializerMethodField()
+
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+    def get_sale_contact(self, instance):
+        sale = instance.sale_contact
+        serializer = DetailCustomUserSerializer(sale)
+        return serializer.data
--- apps/API/views.py
+++ apps/API/views.py
@@ -1,10 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
-from .serializers import CustomerSerializer
+from .serializers import CreateCustomerSerializer, ListCustomersSerializer, \
+    DetailCustomersSerializer
 from .models import Customer
 
 
 class CustomersViewset(ModelViewSet):
-    serializer_class = CustomerSerializer
+    serializer_class = ListCustomersSerializer
+    create_serializer_class = CreateCustomerSerializer
+    detail_serializer_class =DetailCustomersSerializer
 
     def get_queryset(self):
         return Customer.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return self.create_serializer_class
+        elif self.action == 'retrieve':
+            return self.detail_serializer_class
